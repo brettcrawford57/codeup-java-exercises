b603f86ce4d9b661a2b369a9313efec56151ebda
Brett Crawford <brett.crawford57@gmail.com>
2021-04-23 12:28:59 -0500

    exercise question 8

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,30 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+    int myFavoriteNumber = 7;
+    System.out.println(myFavoriteNumber);
+
+
+    String myString = "A little nonsense now and then is relished by the wisest men.";
+    System.out.println(myString);
+
+//    myString = 'e';
+//    //String must be changed to char
+//
+//    myString = 3.14159;
+//    //String must be changed to double
+
+        long myNumber =
+//                System.out.println(myNumber);
+        //myNumber not initialized
+
+//        myNumber = 3.14;
+        //long must be changed to double due to decimal
+
+        myNumber = 123L;
+
+        myNumber = 123;
+
     }
 }
