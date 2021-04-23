29f19767721982692430628c47faf5246a073374
Brett Crawford <brett.crawford57@gmail.com>
2021-04-23 17:07:25 -0500

    ConsoleIO Lecture

---
+++ src/ConsoleIOLecture.java
@@ -0,0 +1,24 @@
+public class ConsoleIOLecture {
+    public static void main(String[] args){
+        System.out.println("Console IO Lecture");
+
+        System.out.println("here");
+        System.out.println("there");
+
+        System.out.print("here\n");
+        System.out.print("there");
+
+        System.out.print("here");
+        System.out.print("there");
+
+        String firstName = "Brett";
+        String lastName = "Crawford";
+        System.out.println(firstName + " " + lastName);
+
+
+
+
+
+
+    }
+}
