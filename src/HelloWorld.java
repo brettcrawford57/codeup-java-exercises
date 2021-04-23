c9ce7bfb373c9b7e82cc1265039c4f2ebdc72684
Brett Crawford <brett.crawford57@gmail.com>
2021-04-23 15:48:14 -0500

    finished exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -15,16 +15,37 @@ public class HelloWorld {
 //    myString = 3.14159;
 //    //String must be changed to double
 
-        long myNumber =
+//        long myNumber;
 //                System.out.println(myNumber);
         //myNumber not initialized
 
 //        myNumber = 3.14;
-        //long must be changed to double due to decimal
+//        long must be changed to double due to decimal
 
-        myNumber = 123L;
+//        myNumber = 123L;
 
-        myNumber = 123;
+//        myNumber = 123;
+
+//        float myNumber;
+//        myNumber = (float)3.14; //or
+//        myNumber = 3.14F;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        int class;
+//        //wont work because class is a keyword
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        //cant cast strings to ints
+
 
     }
 }
