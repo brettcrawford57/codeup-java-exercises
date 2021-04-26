45be21a9f05816301b2aec0db24b1f54ae29b016
Brett Crawford <brett.crawford57@gmail.com>
2021-04-26 17:19:12 -0500

    temp push

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
-
-        System.out.println(pi);
+        System.out.print("The value of pi is approximately " + pi);
     }
 }
