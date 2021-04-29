947f818e330890c62ce377507e17f3e841e46eef
Brett Crawford <brett.crawford57@gmail.com>
2021-04-29 16:49:58 -0500

    unfinished bob and strings

---
+++ src/Bob.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("I'm Bob. 'Sup?");
+            String userInput = scanner.next();
+        }while (){
+
+        });
+            System.out.println("Sure...");
+//        continue;
+//        if (userInput.endsWith("!"))
+//            System.out.println("Dude, you need to chill");
+
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String test = "We don't need no education";
+        System.out.println(test);
+        System.out.println(test.replace("education", "thought control" ));
+
+    }
+}
