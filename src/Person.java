dcabbfec12eef1c9a63dbc60676dcb097af3225b
Brett Crawford <brett.crawford57@gmail.com>
2021-05-03 16:59:35 -0500

    Array study material

--- src/Person.java
+++ src/Person.java
@@ -7,6 +7,7 @@ class Person {
 
     public Person(String personName) {
         name = personName;
+    }
 
     public void setName(String name){
         this.name = name;
@@ -17,15 +18,33 @@ class Person {
     }
 
     public static void main(String[] args) {
-        Person brett = new Person();
-        brett.name = "Brett Crawford";
-        System.out.println(sayHello());
+        Person brett = new Person("John");
+        System.out.println(brett.getName());
+
+        brett.sayHello();
+
+        brett.setName("Brett");
+        System.out.println(brett.getName());
+
+        brett.sayHello();
+
 
 //        Person person1 = new Person("John");
 //        Person person2 = new Person("John");
 //        System.out.println(person1.getName().equals(person2.getName()));
 //        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
     }
-
-
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,41 @@
-package util;
-
-public class Input {
-    private
-}
+//package util;
+//
+//import java.util.Scanner;
+//
+//public class Input {
+//
+//    private Scanner scanner = new Scanner(System.in);
+//
+//    String getString(){
+//
+//    }
+//
+//    boolean yesNo(){
+//
+//    }
+//
+//    public int getInt(int min, int max){
+//            int userInput = scanner.nextInt();
+//            if (userInput > min && userInput < max){
+//                System.out.println("Correct number " + userInput);
+//            } else {
+//                System.out.println("Please enter different number");
+//                return getInt(min, max);
+//            }
+//    }
+//
+//    int getInt(){
+//        return scanner.nextInt();
+//    }
+//    double getDouble(double min, double max){
+//
+//    }
+//    double getDouble() {
+//
+//        }
+//
+//    public static void main(String[] args){
+//
+//    }
+//
+//}
