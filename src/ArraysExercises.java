cbcb8ebeb7ee98a7075887b980a7131d40266511
Brett Crawford <brett.crawford57@gmail.com>
2021-05-04 09:51:56 -0500

    morning push

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,16 +3,25 @@ import java.util.Arrays;
 public class ArraysExercises {
 
 
-public static void main(String[] args){
+    public static void main(String[] args) {
 
-    int[] numbers = {1, 2, 3, 4, 5};
-    System.out.println(Arrays.toString(numbers));
-    }
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
 
-    String[] people = {"James", "John", "Paul"};
-    System.out.println(people[1]);
 
-//    for (int i = 0; i < people.length; i += 1) {
-//        System.out.println(people[i]);
+        String[] Person = {"James", "John", "Paul"};
+        System.out.println(Person[1]);
+
+        for (int i = 0; i < Person.length; i += 1) {
+            System.out.println(Person[i]);
+        }
+
+        public static String addPerson(String Person, person) {
+            return String.format("Hello, %s!", name);
+        }
+
+
+
+
     }
 }
