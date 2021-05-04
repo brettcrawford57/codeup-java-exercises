340bb47e76262fc330277a5036b2bc6aeea5ab1f
Brett Crawford <brett.crawford57@gmail.com>
2021-05-04 11:27:44 -0500

    array exercises 1 and 2 complete

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,26 +2,33 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
 
+        Person[] persons = new Person[3];
+        persons[0] = new Person("John");
+        persons[1] = new Person("Paul");
+        persons[2] = new Person("James");
+        System.out.println(persons[1].getName());
 
-        String[] Person = {"James", "John", "Paul"};
-        System.out.println(Person[1]);
-
-        for (int i = 0; i < Person.length; i += 1) {
-            System.out.println(Person[i]);
+        for (int i = 0; i < persons.length; i += 1) {
+            System.out.println(persons[i].getName());
         }
 
-        public static String addPerson(String Person, person) {
-            return String.format("Hello, %s!", name);
+        Person brett = new Person("Brett");
+        addPerson(persons, brett);
+
+        for (int i = 0; i < addPerson(persons, brett).length; i += 1) {
+            System.out.println(addPerson(persons, brett)[i].getName());
         }
+    }
 
-
-
-
+    public static Person[] addPerson(Person[] people, Person person) {
+        Person[] newPersons = Arrays.copyOf(people, people.length + 1);
+        newPersons[newPersons.length - 1] = person;
+        return newPersons;
     }
 }
+
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -2,6 +2,13 @@ public class ServerNameGenerator {
 
     public static void main(String[] args){
 
+        String[] adjectives = {"high", "small", "red", "cold", "empty", "toxic", "confused", "blind", "sleepy", ""};
+        String[] nouns = {"nebula", "television", "bottle", "fairy", "sword", "star", "box", "fighter", "comet", "meteor"};
 
+        System.out.println("Here is your server name: " + element(adjectives) + "-" + element(nouns));
+    }
+
+    public static String element(String[] word){
+        return word[(int) (Math.random() * (word.length-1))];
     }
 }
