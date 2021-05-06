c24bf99e171b2004b9afec74849b8efab330190b
Brett Crawford <brett.crawford57@gmail.com>
2021-05-06 10:27:55 -0500

    finished circle exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,36 @@
 package shapes;
 
+import java.util.Scanner;
+
 public class Circle {
+
+    public static void main(String[] args){
+
+        Circle newCircle = new Circle();
+
+        System.out.println("You have entered: " + newCircle.radius);
+        System.out.println("This is your circumference: " + newCircle.circumference());
+        System.out.println("This is your area: " + newCircle.getArea());
+
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    private double radius;
+
+    public Circle(){
+
+        System.out.println("Input radius");
+
+        radius = scanner.nextInt();
+    }
+
+    public double circumference(){
+        return (Math.PI * 2) * radius;
+    }
+
+    public double getArea(){
+
+        return (Math.PI * Math.pow(radius, 2));
+    }
 }
