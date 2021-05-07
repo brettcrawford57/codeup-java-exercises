04aad08b14f884924530152180410608f02ac272
Brett Crawford <brett.crawford57@gmail.com>
2021-05-07 16:07:45 -0500

    breaking code: collections exercises

--- src/CollectionsPractice.java
+++ src/CollectionsPractice.java
@@ -1,21 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
+import java.util.*;
 
 public class CollectionsPractice {
 
-    public static upperLastElement(ArrayList<String> strings) {
-        for (String str : strings) {
-            System.out.println(str.toUpperCase());
-        }
-    }
+//    public static upperLastElement(ArrayList<String> strings) {
+//        for (String str : strings) {
+//            System.out.println(str.toUpperCase());
+//        }
+//    }
 
         public static void main (String[] args){
 
-            ArrayList<String> names1 = new ArrayList<>(Arrays.asList("Justin", "Douglas", "Kenneth"));
+//            ArrayList<String> names1 = new ArrayList<>(Arrays.asList("Justin", "Douglas", "Kenneth"));
+//
+//            System.out.println(names1.upperLastElement("Kenneth"));
 
-            System.out.println(names1.upperLastElement("Kenneth"));
+            Map<String, String> classMates = new HashMap<>();
+
+                    classMates.put("Crawford", "Brett");
+                    classMates.put("Howell", "Kenneth");
+                    classMates.put("Adcock", "Nathan");
+
+                    System.out.println(classMates);
 
 
-    }
+            Map<String, List<String>> classMatesWithStuff = new HashMap<>();
+            classMatesWithStuff.put("Shanshan",new ArrayList<>());
+            classMatesWithStuff.put("Alex", new ArrayList<>());
+
+            System.out.println(classMatesWithStuff);
+
+
+        }
 }
---
+++ src/grades/Student.java
@@ -0,0 +1,32 @@
+package grades;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+
+    private String name;
+
+    private ArrayList<Integer> grades;
+
+    public Student(String name, int grades) {
+        this.name = name;
+        grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+        return;
+
+        public double getGradeAverage(){
+            return grades / grade.size
+        }
+    }
+
+    HashMap<String, String> students = new HashMap<>();
+
+}
